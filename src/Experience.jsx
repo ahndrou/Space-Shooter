3e9398f5b0c1ea28596c5c63df301409a36c2b38
Andrew <andrew.alan.sm@gmail.com>
2026-02-09 12:41:35 +0000

    add keyboard controls.
    control scheme is a basic one for testing. world space rotations are accounted for when moving forward.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,21 +2,28 @@ import { Canvas } from "@react-three/fiber";
 import Spaceship from "./Spaceship";
 import { Physics } from "@react-three/rapier";
 import Level from "./Level";
-import { OrbitControls } from "@react-three/drei";
+import { KeyboardControls, OrbitControls } from "@react-three/drei";
 import Lights from "./Lights";
 
 export default function Experience() {
     return (
         <>
-            <Canvas shadows>
-                <OrbitControls />
+            <KeyboardControls
+                map={[
+                    {name: 'forward', keys: ['ArrowUp', 'KeyW']},
+                    {name: 'leftward', keys: ['ArrowLeft', 'KeyA']},
+                    {name: 'rightward', keys: ['ArrowRight', 'KeyD']}
+                ]}>
+                <Canvas shadows>
+                    <OrbitControls />
 
-                <Lights />
-                <Physics debug>
-                    <Spaceship />
-                    <Level />
-                </Physics>
-            </Canvas>
+                    <Lights />
+                    <Physics debug>
+                        <Spaceship />
+                        <Level />
+                    </Physics>
+                </Canvas>
+            </KeyboardControls>
         </>
         
     )
--- src/Spaceship.jsx
+++ src/Spaceship.jsx
@@ -1,12 +1,38 @@
+import { useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
+import { Quaternion, Vector3 } from "three";
 
 export default function Spaceship() {
     const rb = useRef()
 
-    useEffect(() => {
-        rb.current.setLinvel({x: 0, y: 0, z: -1}, true)
-    }, [])
+    const [ subToKeys, getKeys ] = useKeyboardControls()
+
+    useFrame(() => {
+        const keysState = getKeys()
+
+        if (keysState.forward) {
+            const localSpaceImpulse = new Vector3(0, 0, -0.75)
+
+            const worldSpaceOrientation = new Quaternion(
+                rb.current.rotation().x,
+                rb.current.rotation().y,
+                rb.current.rotation().z,
+                rb.current.rotation().w,
+            ) 
+            const worldSpaceImpulse = localSpaceImpulse.applyQuaternion(worldSpaceOrientation)
+            rb.current.applyImpulse(worldSpaceImpulse, true)
+        }
+
+        if (keysState.leftward) {
+            rb.current.applyTorqueImpulse({ x: 0, y: 0.4, z: 0})
+        }
+        
+        if (keysState.rightward) {
+            rb.current.applyTorqueImpulse({ x: 0, y: -0.4, z: 0})
+        }
+    })
 
     return (
         <RigidBody ref={rb} position={[0, 4, 0]}>
