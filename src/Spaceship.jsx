74c7fc7faa52c49216d2a80fe61a479ad22f2e22
Andrew <andrew.alan.sm@gmail.com>
2026-02-09 14:47:53 +0000

    implement third person camera.
    camera follows the rotation of the ship.

--- src/Spaceship.jsx
+++ src/Spaceship.jsx
@@ -1,13 +1,16 @@
 import { useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Quaternion, Vector3 } from "three";
 
 export default function Spaceship() {
     const rb = useRef()
 
     const [ subToKeys, getKeys ] = useKeyboardControls()
+
+    const [cameraPosition] = useState(() => new Vector3(0, 0, 0))
+    const [cameraTarget] = useState(() => new Vector3(0, 0, 0))
 
     useFrame(() => {
         const keysState = getKeys()
@@ -32,6 +35,31 @@ export default function Spaceship() {
         if (keysState.rightward) {
             rb.current.applyTorqueImpulse({ x: 0, y: -0.4, z: 0})
         }
+    })
+
+    // CAMERA HANDLING
+    useFrame((state) => {
+        cameraPosition.copy(rb.current.translation())
+        cameraTarget.copy(rb.current.translation())
+        
+        let positionTransform = new Vector3(0, 2, 6)
+        let targetTransform = new Vector3(0, 0, -6)
+        
+        // Transforming from local to world space.
+        const shipOrientation = new Quaternion(
+            rb.current.rotation().x,
+            rb.current.rotation().y,
+            rb.current.rotation().z,
+            rb.current.rotation().w,
+        )
+        positionTransform.applyQuaternion(shipOrientation)
+        targetTransform.applyQuaternion(shipOrientation)
+
+        cameraPosition.add(positionTransform)
+        cameraTarget.add(targetTransform)
+
+        state.camera.position.copy(cameraPosition)
+        state.camera.lookAt(cameraTarget)
     })
 
     return (
