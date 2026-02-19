f70ef7fd76bfbb5e3718175a431ba3bf126b9dd5
Andrew <andrew.alan.sm@gmail.com>
2026-02-19 19:46:44 +0000

    improve movement.
    added linear velocity handling, as well as refined mouse controls for pitch and yaw.

--- src/Spaceship.jsx
+++ src/Spaceship.jsx
@@ -2,12 +2,13 @@ import { useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
 import { useRef } from "react";
-import { Euler, Quaternion, Vector3 } from "three";
-
-
+import { Quaternion, Vector3 } from "three";
 
 export default function Spaceship() {
-    const ANGULAR_SPEED_FACTOR = 1
+    const ANGULAR_SPEED_FACTOR = 0.9
+    const LINEAR_SPEED_FACTOR = 6
+    const POINTER_LOWER_BOUND = 0.3
+    const POINTER_UPPER_BOUND = 0.99
     const LINEAR_DAMPING = 0.4
     const ANGULAR_DAMPING = 0.6
 
@@ -18,8 +19,9 @@ export default function Spaceship() {
     const cameraOffset = new Vector3()
     const worldSpaceRotation = new Quaternion()
     const angularVelocity = new Vector3(0, 0, 0)
+    const linearVelocity = new Vector3(0, 0, 0)
 
-    useFrame((state, delta) => {
+    useFrame((state) => {
         const keys = getKeys()
 
         // rb.current.rotation() returns a plain object, not an instance
@@ -32,7 +34,7 @@ export default function Spaceship() {
         )
 
         // CAMERA SETUP
-        cameraOffset.set(0, 3, 6)
+        cameraOffset.set(0, 1, 7)
         cameraOffset.applyQuaternion(worldSpaceRotation)
         
         state.camera.position.copy(rb.current.translation())
@@ -41,14 +43,32 @@ export default function Spaceship() {
         state.camera.rotation.setFromQuaternion(worldSpaceRotation)
 
         // INPUT HANDLING
-        const yawSpeed = ((keys.leftward ? 1 : 0) + (keys.rightward ? -1 : 0)) * 2
-        const pitchSpeed = ((keys.forward ? 1 : 0) + (keys.backward ? -1 : 0)) * 2
-        const rollSpeed = (keys.space ? 1 : 0) * 2
+        let yawSpeed = 0
+        // Upper bound is to prevent endless spinning if user moves pointer outside of the window.
+        if (Math.abs(state.pointer.x) > POINTER_LOWER_BOUND && Math.abs(state.pointer.x) < POINTER_UPPER_BOUND) {
+            yawSpeed = -state.pointer.x * ANGULAR_SPEED_FACTOR
+        }
+        
+        let pitchSpeed = 0
+        if (Math.abs(state.pointer.y) > POINTER_LOWER_BOUND && Math.abs(state.pointer.y) < POINTER_UPPER_BOUND) {
+            pitchSpeed = state.pointer.y * ANGULAR_SPEED_FACTOR
+        }
+
+        const rollSpeed = ((keys.leftward ? 1 : 0) + (keys.rightward ? -1 : 0)) * ANGULAR_SPEED_FACTOR
 
         angularVelocity.set(pitchSpeed, yawSpeed, rollSpeed)
         angularVelocity.applyQuaternion(worldSpaceRotation)
 
         rb.current.setAngvel(angularVelocity, true)
+
+        linearVelocity.set(
+            0,
+            0,
+            keys.forward ? -LINEAR_SPEED_FACTOR : 0
+        )
+        linearVelocity.applyQuaternion(worldSpaceRotation)
+        rb.current.setLinvel(linearVelocity)
+
     })
 
     return (
