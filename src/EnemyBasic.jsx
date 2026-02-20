d017658fa8b4f91dec2b3794aa7e5b6777151e9e
Andrew <andrew.alan.sm@gmail.com>
2026-02-20 23:48:10 +0000

    add random angular velocity to each basic enemy

--- src/EnemyBasic.jsx
+++ src/EnemyBasic.jsx
@@ -1,16 +1,28 @@
 import { useGLTF } from "@react-three/drei";
 import { RigidBody } from "@react-three/rapier";
+import { useEffect, useRef } from "react";
+import { Quaternion, Vector3 } from "three";
 
 export default function EnemyBasic({position}) {
     const gltf = useGLTF("./space_shooter_enemy_basic.glb")
-    
+    const rb = useRef()
 
-    console.log(gltf.materials["Material.003"])
+    const MAX_SPEED = 0.4
+
+    useEffect(() => {
+        console.log(rb.current)
+        rb.current.setAngvel(new Vector3(
+            Math.min(Math.random(), MAX_SPEED),
+            Math.min(Math.random(), MAX_SPEED),
+            Math.min(Math.random(), MAX_SPEED)
+        ))
+    }, [])
 
     return (
         <RigidBody
-            type="kinematicPosition"
+            type="kinematicVelocity"
             position={position}
+            ref={rb}
             >
                 <group scale={3}>
                     <mesh 
