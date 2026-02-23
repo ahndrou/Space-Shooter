a2bf17121b1d8e6152f7cb19ad745d3c5bbc138f
Andrew <andrew.alan.sm@gmail.com>
2026-02-23 20:15:50 +0000

    create square grid effect in bounding area shader.

--- src/shaders/boundary/fragment.glsl
+++ src/shaders/boundary/fragment.glsl
@@ -3,9 +3,17 @@ precision mediump float;
 varying vec2 vUv;
 
 void main() {
-    float strength = 1.0 - step(0.495, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
-    vec4 colour = vec4(1.0, 0, 0, 0.99);
-    vec4 baseColour = vec4(0.5, 0.5, 1.0, 0.9);
+    float border = step(0.495, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+
+    float horizontalLines = step(0.3, mod(vUv.y * 260.0, 1.0));
+    float verticalLines = step(0.3, mod(vUv.x * 260.0, 1.0));
+    float dots = horizontalLines * verticalLines;
+
+    float strength = border + dots;
+    strength = clamp(strength, 0.0, 1.0);
+
+    vec4 detailColour = vec4(0.9, 0.2, 0.8, 0.4);
+    vec4 baseColour = vec4(0.2, 0.2, 0.2, 0.1);
     
-    gl_FragColor = mix(colour, baseColour, strength);
+    gl_FragColor = mix(baseColour, detailColour, strength);
 }
