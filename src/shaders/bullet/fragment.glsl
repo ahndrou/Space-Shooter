05d25bcc32a244e72a6f1e98009bb9c68617d152
Andrew <andrew.alan.sm@gmail.com>
2026-02-26 18:26:34 +0000

    add glowing orb shader effect for bullets

--- src/shaders/bullet/fragment.glsl
+++ src/shaders/bullet/fragment.glsl
@@ -3,5 +3,11 @@ precision mediump float;
 varying vec2 vUv;
 
 void main() {
-    gl_FragColor = vec4(1.0);
+    float strength = pow(0.016, (distance(vUv, vec2(0.5))));
+
+    vec3 highlightColour = vec3(0.9, 0.99, 0.9);
+
+    if (strength < 0.2) discard;
+
+    gl_FragColor = vec4(highlightColour, strength);
 }
