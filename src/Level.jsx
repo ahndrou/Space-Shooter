2461cc738adf15733f3e4a112858f7f9f349a9b6
Andrew <andrew.alan.sm@gmail.com>
2026-03-13 12:11:26 +0000

    simplify initial spawning logic

--- src/Level.jsx
+++ src/Level.jsx
@@ -3,79 +3,66 @@ import EnemyBasic from "./EnemyBasic";
 import { Vector3 } from "three";
 import { generateUUID } from "three/src/math/MathUtils.js";
 
-const ENEMY_COUNT = 600
+const ENEMY_COUNT = 200
 const ENEMY_SIZE = 6
 
-function createEnemyPosition(boundingDimensions, enemySize, positionJitterFactor) {
-    const cellSize = enemySize * positionJitterFactor + 1
+function createEnemyPosition(boundingDimensions, enemySize) {
+    let x = (Math.random() - 0.5) * (boundingDimensions.x - enemySize / 2)
+    let y = (Math.random() - 0.5) * (boundingDimensions.y - enemySize / 2)
+    let z = (Math.random() - 0.5) * (boundingDimensions.z - enemySize / 2)
 
-    let x = (Math.random() - 0.5) * (boundingDimensions.x - cellSize)
-    x = Math.round(x / cellSize) * cellSize
-    
-    let y = (Math.random() - 0.5) * (boundingDimensions.y - cellSize)
-    y = Math.round(y / cellSize) * cellSize
-
-    let z = (Math.random() - 0.5) * (boundingDimensions.z - cellSize)
-    z = Math.round(z / cellSize) * cellSize
-
-    return [x, y, z]
-}
-
-function includesChildArray(array, child) {
-    return array.some((element => 
-        element.length === child.length &&
-        element[0] === child[0] &&
-        element[1] === child[1] &&
-        element[2] === child[2] 
-    ))
+    return new Vector3(x, y, z)
 }
 
 // Creates a set of initial positions which do not cause intersection of enemies.
-function createInitialPositions(boundingDimensions, enemySize, positionJitterFactor) {
+function createInitialPositions(boundingDimensions, enemySize) {
     const enemyPositions = []
+
+    // O(n^2). Okay here considering it is a one off and number of enemies should be relatively low.
     for (let i = 0; i < ENEMY_COUNT; i++) {
-        let position = createEnemyPosition(boundingDimensions, enemySize, positionJitterFactor)
+        let attempts = 0
+        while (attempts < 100) {
+            let newPosition = createEnemyPosition(boundingDimensions, enemySize)
+            let intersectionFound = false
 
-        let timeoutCounter = 0
-        while (timeoutCounter < 500 && includesChildArray(enemyPositions, position)) {
-            position = createEnemyPosition(boundingDimensions, enemySize, positionJitterFactor)
-            timeoutCounter++
+            for (let existingPosition of enemyPositions) {
+                if (newPosition.distanceTo(existingPosition) <= (enemySize + 0.05)) {
+                    intersectionFound = true
+                    break
+                }
+            }
+
+            if (!intersectionFound) {
+                enemyPositions.push(newPosition)
+                break
+            } else {
+                attempts++
+            }
         }
 
-        if (timeoutCounter === 500) {
-            throw new Error(`Could not find empty cell for enemy. Play area is likely too small for given combination of enemy size, count and jitter factor.`)
+        if (attempts === 100) {
+            throw new Error(`Could not find empty cell for enemy. Play area is likely too small for given combination of enemy size and count.`)
         }
-
-        enemyPositions.push(position)
     }
 
     return enemyPositions
 }
 
-function createInitialEnemyData(boundingDimensions, enemySize, positionJitterFactor) {
-    const positions = createInitialPositions(boundingDimensions, enemySize, positionJitterFactor)
-
-    const maxOffset = enemySize * positionJitterFactor - enemySize
-    const jitteredPositions = positions.map(([x, y, z]) => 
-        [
-            x + (Math.random() - 0.5) * maxOffset,
-            y + (Math.random() - 0.5) * maxOffset,
-            z + (Math.random() - 0.5) * maxOffset
-        ])
-    
-    return jitteredPositions.map(([x, y, z]) => {return {id: generateUUID(), position: new Vector3(x, y, z)}})
+function createInitialEnemyState(boundingDimensions, enemySize) {
+    const positions = createInitialPositions(boundingDimensions, enemySize)
+    return positions.map((position) => {return {id: generateUUID(), position}})
 }
 
 export default function Level({playAreaBounds}) {
     
     const [enemies, setEnemies] = useState(() => {
-        return createInitialEnemyData(playAreaBounds, ENEMY_SIZE, 1.5)
+        return createInitialEnemyState(playAreaBounds, ENEMY_SIZE)
     })
 
     const removeEnemy = useCallback((enemyId) => {
         setEnemies((enemies) => enemies.filter((enemy) => enemy.id !== enemyId))
     }, [setEnemies])
-
+    
     return (
         enemies.map((enemyData) => {
             return (
