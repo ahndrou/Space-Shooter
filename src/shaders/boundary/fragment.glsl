61d18b77afc6bdc19ad54b804c1943a4fc1a961d
Andrew <andrew.alan.sm@gmail.com>
2026-03-26 19:37:45 +0000

    add fade out at the edge of hex grid shader to hide misalignmented between adjacent walls

--- src/shaders/boundary/fragment.glsl
+++ src/shaders/boundary/fragment.glsl
@@ -22,6 +22,9 @@ float HexDist(vec2 p) {
     return c;
 }
 
+// x is a polar coordinate of the given point within a hexagon, 
+// y is distance to the hexagon boundary,
+// z and w are cell ID coordinates.
 vec4 HexCoords(vec2 uv) {
 	vec2 r = vec2(1, 1.73);
     vec2 h = r*.5;
@@ -40,14 +43,23 @@ vec4 HexCoords(vec2 uv) {
 void main() {
     vec3 col = vec3(0.);
 
-    vec2 uv = vUv * 35.0;
+    // Factor here controls the scale of the grid.
+    vec2 uv = 2.0 * (vUv - 0.5) * 20.0;
 
     vec4 hc = HexCoords(uv);
 
+    // r controls the thickness of the hexagon borders.
     float r = 0.11;
     float c = smoothstep(r, r + 0.01, hc.y);
     
     col += c;
 
-    gl_FragColor = vec4(col, 0.3);
+    // b controls the fade-out distance at the edge of the grid.
+    float b = 0.15;
+    vec2 strengthV = abs(2.0 * (vUv - 0.5));
+    strengthV = smoothstep(0.99, 0.99 - b, strengthV);
+
+    float strength = 0.1 * min(strengthV.x, strengthV.y);
+
+    gl_FragColor = vec4(col, strength);
 }
